c5910e054fb3e8dc15b99b3c32cd62408e0d81d0
matxx <matxx.webmaster@gmail.com>
2025-08-24 22:03:29 +0200

    chore: new HoF links

--- utils/constants.ts
+++ utils/constants.ts
@@ -40,10 +40,10 @@ export const HALL_OF_FORMS_RESOURCES = {
   PM1: null,
   // PM1: 'https://www.youtube.com/watch?v=sjWbHsDwykw',
   // FEHOLOGY: null,
-  FEHOLOGY: 'https://www.youtube.com/watch?v=QWGz6xMm5yE',
+  FEHOLOGY: 'https://www.youtube.com/watch?v=708h2Giu1-w',
   // SATACHI: null,
   // SATACHI: 'https://www.youtube.com/watch?v=RbMUtD7uuM8',
   // COOLEST_MAGICAL_CAT: null,
   COOLEST_MAGICAL_CAT:
-    'https://www.reddit.com/r/FireEmblemHeroes/comments/1m7c38r/recommended_forma_skills_infographic_hall_of/',
+    'https://www.reddit.com/r/FireEmblemHeroes/comments/1mz3b7x/recommended_forma_skills_infographic_hall_of/',
 }
