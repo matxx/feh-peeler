c20874f954af1e1d144599a5561037f2657cf02a
matxx <matxx.webmaster@gmail.com>
2025-12-15 20:27:35 +0100

    chore: 2025-12-15 update

--- composables/useData.ts
+++ composables/useData.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nuxt'
 
 const DOMAIN = 'data.feh-peeler.com'
-const COMMIT = 'b6a5775c5ca1253913af24f62bf34e5c867c1244'
+const COMMIT = 'edf59730acaa8f04db7cf463c2a03892b160d4c4'
 
 export default function useData(
   filename: string,
