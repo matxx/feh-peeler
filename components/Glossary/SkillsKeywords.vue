cd42709f58b8679cc12bcefd3539e1b51e2bdede
matxx <matxx.webmaster@gmail.com>
2026-01-29 20:36:54 +0100

    feat(glossary->keywords): no minimal length search

--- components/Glossary/SkillsKeywords.vue
+++ components/Glossary/SkillsKeywords.vue
@@ -11,8 +11,6 @@
               label="Name"
               clearable
               density="compact"
-              :counter="counter"
-              :color="searchIsActive ? 'success' : 'primary'"
               :error-messages="errorMessages"
             />
           </th>
@@ -29,7 +27,7 @@
           v-for="(line, index) in lines"
           :key="index"
           :text="line"
-          :regexp="searchIsActive ? regexp : undefined"
+          :regexp="regexp"
         />
       </tbody>
     </v-table>
@@ -37,24 +35,11 @@
 </template>
 
 <script setup lang="ts">
-import { MINIMAL_TEXT_SEARCH_LENGTH } from '~/utils/constants'
-
 const { t } = useI18n()
 const { mobile } = useDisplay()
 const storeDataConstants = useStoreDataConstants()
 const lines = computed(() => storeDataConstants.constants?.keywords || [])
 
 const searchText = ref('')
-const searchLength = computed(() =>
-  searchText.value ? searchText.value.length : 0,
-)
-const counter = computed(() =>
-  searchLength.value <= MINIMAL_TEXT_SEARCH_LENGTH
-    ? MINIMAL_TEXT_SEARCH_LENGTH
-    : undefined,
-)
-const searchIsActive = computed(
-  () => searchLength.value >= MINIMAL_TEXT_SEARCH_LENGTH,
-)
 const { regexp, errorMessages } = useSearch(searchText)
 </script>
