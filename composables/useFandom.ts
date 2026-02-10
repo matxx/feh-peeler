d318a245df04b5bc8d9a39cd4c76efbcee4fbdb8
matxx <matxx.webmaster@gmail.com>
2026-02-10 01:50:32 +0100

    fix: duplicate exports

--- composables/useFandom.ts
+++ composables/useFandom.ts
@@ -1,6 +1,6 @@
 import type { FandomId } from '~/utils/types/fandom'
 
-export const BASE_URL = 'https://feheroes.fandom.com/wiki/'
+const BASE_URL = 'https://feheroes.fandom.com/wiki/'
 
 export default function () {
   const escapeString = (str: string) => str.replace(/[ /]/g, '_')
--- composables/useGuide.ts
+++ composables/useGuide.ts
@@ -1,6 +1,6 @@
 import type { GuideId } from '~/utils/types/guide'
 
-export const BASE_URL = 'https://guide.fire-emblem-heroes.com/en-US/'
+const BASE_URL = 'https://guide.fire-emblem-heroes.com/en-US/'
 
 export default function () {
   const l = (id: GuideId) => `${BASE_URL}${id}`
