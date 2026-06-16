808497b6c6e67ff8fbd482247c4f6b2a40ea7155
matxx <matxx.webmaster@gmail.com>
2026-06-16 14:54:09 +0200

    revert: UI shift to the bottom after mounted

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,9 @@
 <template>
   <v-app>
-    <AppHeader
-      v-model:is-drawer-open="isDrawerOpen"
-      :class="{ 'v-head--before-mounted': !mounted }"
-    />
+    <AppHeader v-model:is-drawer-open="isDrawerOpen" />
     <AppNavigationDrawer v-model:is-open="isDrawerOpen" />
 
-    <v-main :class="{ 'v-main--before-mounted': !mounted }">
+    <v-main>
       <slot />
     </v-main>
 
@@ -19,7 +16,6 @@
 <script setup lang="ts">
 const { locale } = useI18n()
 const storeTheme = useStoreTheme()
-const { mounted } = useMounted()
 const isDrawerOpen = ref(false)
 useDataStores([useStoreDataAccents()])
 useHead(() => ({
@@ -33,23 +29,3 @@ useHead(() => ({
 }))
 onMounted(useStoreGlobals().updateScrollbarSizes)
 </script>
-
-<style lang="scss" scoped>
-/* MONKEY PATCH: before mounted, padding for header is not set causing some UI shifting to the bottom at mount time */
-.v-main--before-mounted {
-  padding-top: 64px;
-}
-
-.v-head--before-mounted {
-  z-index: 1006;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-
-  &::v-deep .v-toolbar__content {
-    align-items: center;
-    display: flex;
-  }
-}
-</style>
