3b6d5b420554f992099de7093baf9bd0b3096fe1
matxx <matxx.webmaster@gmail.com>
2026-07-22 14:30:11 +0200

    feat: adding sprite sheet `Mjolnir_Common`

---
+++ components/App/SpriteSheets/Mjolnir_Common.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppSpriteSheet
+    :sprite-sheet="spriteDataRaw"
+    :img="spriteImg"
+    :name="frame"
+    v-bind="props"
+  />
+</template>
+
+<script setup lang="ts">
+import spriteDataRaw from '~/assets/sprite-sheets/Mjolnir_Common.json'
+import spriteImg from '~/assets/sprite-sheets/Mjolnir_Common.png'
+
+const props = defineProps<{
+  frame: string
+  width?: number
+  height?: number
+  size?: number
+}>()
+</script>
